68193255103f8a24d334aef93107c4cef078a590
Elisha Adeboye <y.adeboye@gmail.com>
2025-01-19 12:43:27 +0100

    moved questions to trivia component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import "./App.css";
-import { useEffect, useMemo, useState } from "react";
+// import { useEffect, useMemo, useState } from "react";
+import { useState } from "react";
 import Start from "./components/Start";
 import Timer from "./components/Timer";
 import Trivia from "./components/Trivia";
 import Testin from "./components/Testin";
 import { data } from "./components/data";
 import { moneyParam } from "./components/moneyParam";
+import { useGetAllQuestionsQuery } from "./components/dataService/apiData";
 
 function App() {
+  const { data, isError, isLoading } = useGetAllQuestionsQuery();
   const [username, setUsername] = useState(null);
   const [timeOut, setTimeOut] = useState(false);
   const [questionNumber, setQuestionNumber] = useState(1);
@@ -15,80 +18,85 @@ function App() {
   const [apiQuestion, setApiQuestion] = useState({});
   const [transQuestion, setTransQuestion] = useState([]);
 
+  // console.log(data);
+
   //fetch data
-  useEffect(() => {
-    let str2 = "https://opentdb.com/api.php?amount=15&type=multiple";
-    const fetchQuestions = async () => {
-      const response = await fetch(str2);
-      const data = await response.json();
-      setApiQuestion(data.results);
-    };
+  // useEffect(() => {
+  //   let str2 = "https://opentdb.com/api.php?amount=15&type=multiple";
+  //   const fetchQuestions = async () => {
+  //     const response = await fetch(str2);
+  //     const data = await response.json();
+  //     setApiQuestion(data.results);
+  //   };
 
-    fetchQuestions();
-  }, []);
+  //   fetchQuestions();
+  // }, []);
 
-  //transformed fetched data
-  useEffect(() => {
-    let generalArray = [];
+  // //transformed fetched data
+  // useEffect(() => {
+  //   let generalArray = [];
 
-    function escapeSpecialChars(htmlStr) {
-      htmlStr = htmlStr.replace(/&lt;/g, "<");
-      htmlStr = htmlStr.replace(/&gt;/g, ">");
-      htmlStr = htmlStr.replace(/&quot;/g, '"');
-      htmlStr = htmlStr.replace(/&#39;/g, "'");
-      htmlStr = htmlStr.replace(/&#039;/g, "'");
-      htmlStr = htmlStr.replace(/&amp;/g, "&");
-      return htmlStr;
-    }
+  //   function escapeSpecialChars(htmlStr) {
+  //     htmlStr = htmlStr.replace(/&lt;/g, "<");
+  //     htmlStr = htmlStr.replace(/&gt;/g, ">");
+  //     htmlStr = htmlStr.replace(/&quot;/g, '"');
+  //     htmlStr = htmlStr.replace(/&#39;/g, "'");
+  //     htmlStr = htmlStr.replace(/&#039;/g, "'");
+  //     htmlStr = htmlStr.replace(/&amp;/g, "&");
+  //     return htmlStr;
+  //   }
 
-    for (let i = 0; i < apiQuestion.length; i++) {
-      let iteratedQuestion = escapeSpecialChars(apiQuestion[i]?.question);
-      let iteratedAnswer = escapeSpecialChars(apiQuestion[i]?.correct_answer);
-      let wronganswers = apiQuestion[i].incorrect_answers;
+  //   for (let i = 0; i < apiQuestion.length; i++) {
+  //     let iteratedQuestion = escapeSpecialChars(apiQuestion[i]?.question);
+  //     let iteratedAnswer = escapeSpecialChars(apiQuestion[i]?.correct_answer);
+  //     let wronganswers = apiQuestion[i].incorrect_answers;
 
-      let choice = [];
-      for (const x of wronganswers) {
-        choice.push({
-          id: wronganswers.indexOf(x),
-          text: escapeSpecialChars(x),
-          correct: false,
-        });
-      }
+  //     let choice = [];
+  //     for (const x of wronganswers) {
+  //       choice.push({
+  //         id: wronganswers.indexOf(x),
+  //         text: escapeSpecialChars(x),
+  //         correct: false,
+  //       });
+  //     }
 
-      // Add correct option
-      let unshuffledOptions = [
-        ...choice,
-        { id: 3, text: iteratedAnswer, correct: true },
-      ];
-      //shuffled options so answers won't always be at the same position
-      let shuffledOptions = unshuffledOptions
-        .map((value) => ({ value, sort: Math.random() }))
-        .sort((a, b) => a.sort - b.sort)
-        .map(({ value }) => value);
+  //     // Add correct option
+  //     let unshuffledOptions = [
+  //       ...choice,
+  //       { id: 3, text: iteratedAnswer, correct: true },
+  //     ];
+  //     //shuffled options so answers won't always be at the same position
+  //     let shuffledOptions = unshuffledOptions
+  //       .map((value) => ({ value, sort: Math.random() }))
+  //       .sort((a, b) => a.sort - b.sort)
+  //       .map(({ value }) => value);
 
-      // Add each questions
-      let item = {
-        id: `${i}`,
-        question: `${iteratedQuestion}`,
-        answers: shuffledOptions,
-      };
+  //     // Add each questions
+  //     let item = {
+  //       id: `${i}`,
+  //       question: `${iteratedQuestion}`,
+  //       answers: shuffledOptions,
+  //     };
 
-      generalArray.push(item);
-    }
+  //     generalArray.push(item);
+  //   }
 
-    setTransQuestion(generalArray);
-    console.log(generalArray);
-  }, []);
+  //   setTransQuestion(generalArray);
+  //   console.log(generalArray);
+  // }, []);
 
-  const moneyPyramid = useMemo(() => moneyParam, []);
+  // const moneyPyramid = useMemo(() => moneyParam, []);
 
-  useEffect(() => {
-    questionNumber > 1 &&
-      setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
-  }, [questionNumber, moneyPyramid]);
+  // useEffect(() => {
+  //   questionNumber > 1 &&
+  //     setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
+  // }, [questionNumber, moneyPyramid]);
 
   return (
-    <div>We are troubleshooting our app</div>
+    <div>
+      <Trivia />
+      We are troubleshooting our app
+    </div>
 
     // <div className="app">
     //   {!username ? (
